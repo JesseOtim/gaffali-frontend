56a873448aff6c64eb647b6ae91c577963a2ea1a
Jesse Otim <otimdanieljesse@gmail.com>
2023-07-31 14:44:12 +0300

    Added Navbar

--- src/App.js
+++ src/App.js
@@ -1,22 +1,102 @@
 import { Navbar, Nav } from "react-bootstrap";
 import Container from "react-bootstrap/Container";
+import NavDropdown from "react-bootstrap/NavDropdown";
 
-function App() {
+const App = () => {
   return (
-    <>
-     <Navbar bg="dark" data-bs-theme="dark">
+    <div className="App">
+      <Navbar bg="dark" data-bs-theme="dark">
         <Container>
-          <Navbar.Brand href="#home">||| GAFFALI</Navbar.Brand>
+          <Navbar.Brand href="#home" style={{fontWeight:'bold'}}><span style = {{color:'darkgreen'}} >|</span><span style={{color:'orangered'}}>|</span><span style={{color:'grey'}}>|</span> GAFFALI</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav>
+              <NavDropdown
+                id="nav-dropdown-dark-example"
+                title="Landing Page"
+                menuVariant="dark"
+            >
+                <NavDropdown.Item href="#action/3.1">
+                  Landing page 1
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                  Landing page 2
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+
+            <Nav>
+            <NavDropdown
+              id="nav-dropdown-dark-example"
+              title="Dropdown"
+              menuVariant="dark"
+            >
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+
+             <Nav>
+            <NavDropdown
+              id="nav-dropdown-dark-example"
+              title="Dropdown"
+              menuVariant="dark"
+            >
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          <Nav>
+            <NavDropdown
+              id="nav-dropdown-dark-example"
+              title="Dropdown"
+              menuVariant="dark"
+            >
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          <Nav>
+            <NavDropdown
+              id="nav-dropdown-dark-example"
+              title="Dropdown"
+              menuVariant="dark"
+            >
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
           </Nav>
         </Container>
       </Navbar>
-    
-    </>
-     
+    </div>
   );
 }
 
