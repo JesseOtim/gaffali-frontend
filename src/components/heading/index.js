5719967ac4b0ad3f8407b41ec042f27cf21ed5d1
Jesse Otim <otimdanieljesse@gmail.com>
2023-08-01 08:51:07 +0300

    Modified heading component

--- src/components/heading/index.js
+++ src/components/heading/index.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-export default function index() {
+export default function heading() {
   return (
-    <div style={{ display: 'flex', justifyContent: 'center', flexDirection:'column', marginLeft:'450px', marginTop:'120px' }}>
-        <h1>A trusted and secure <br style={{marginRight:'1200px'}}></br>cryptocurrency exchange.</h1>
+    <div style={{textAlign:'center'}}>
+        <h1 style={{marginTop:'120px', fontSize:'55px', fontWeight:'bold'}}>A trusted and secure <br/><span style={{marginRight:'30px'}}>cryptocurrency exchange.</span></h1>
         <p style={{fontWeight:'bold'}}>Crypo is the most advanced UI kit for making the Blockchain platform. This kit comes with 4 different <br></br>exchange page, market, wallet and many more</p>
     </div>
   )
