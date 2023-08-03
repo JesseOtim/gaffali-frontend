71a50eabb0d7be315171d12667f2ab9a4af522d4
Jesse Otim <otimdanieljesse@gmail.com>
2023-08-03 11:10:40 +0300

    Refactor code

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+
+library.add(fas);
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
