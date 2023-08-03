204e62d33c84539ecac2aaf466ed93ae469d2f6f
Jesse Otim <otimdanieljesse@gmail.com>
2023-08-03 14:56:57 +0300

    Refactor code

--- src/components/subHeading/index.js
+++ src/components/subHeading/index.js
@@ -1,8 +1,13 @@
-import React from 'react'
+import React from "react";
 
-export default function 
-subHeading() {
+export default function SubHeading() {
   return (
-    <div><h1></h1></div>
-  )
+    <div style={{textAlign:'center'}}>
+      <h2 style={{marginTop:'100px', color: '#18214D'}}>
+        Here are a few reasons why
+        <br />
+        you should choose Crypo
+      </h2>
+    </div>
+  );
 }
